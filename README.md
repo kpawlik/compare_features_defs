e673c5cf67aed2af6cc15ffa8ea4a1f0ec7f9317
Kris Pawlik <kpawlik78@gmail.com>
2025-04-30 11:35:23 +0200

    compare versions

--- README.md
+++ README.md
@@ -8,43 +8,20 @@
 go run main.go -d1 ./d1 -d2 ./d2 
 ```
 
-Result:
+Result in CSV:
 
 ```bash
 $ go run main.go -d1 ./d1 -d2 ./d2 
-2025/03/03 10:18:36 Comparing ./d1 ==> ./d2
-2025/03/03 10:18:36 Compared files: d1\address.def ==> d2\address.def
-2025/03/03 10:18:36   Field Name                    |Value 1                       |Value 2                       |Description
-2025/03/03 10:18:36 ------------------------------------------------------------------------------------------------------------------------
-2025/03/03 10:18:36   id2                           |                              |                              |Field does not exists in ./d2
-2025/03/03 10:18:36   herkomst                      |string(1)                     |string(11)                    |Different values
-2025/03/03 10:18:36   straat                        |Straat                        |Straata                       |Different values
+Feature,Field,Dir 1,Dir 2,type (Dir 1),type (Dir 2),external_name (Dir 1),external_name (Dir 2)
+address.def,id2,removed,,,,,
+address.def,id3,,added,,,,
+address.def,herkomst,,,string(1),string(11),,
+address.def,straat,,,,,Straat,Straata```
 ```
 
-- both way
+Displays difference what was removed and added between versions d1 and d2
 
-```bash
-go run main.go -d1 ./d1 -d2 ./d2 -bw
-```
-
-Result
-
-```bash
-025/03/03 11:19:14 Comparing ./d1 ==> ./d2
-2025/03/03 11:19:14 Compared files: d1\address.def ==> d2\address.def
-2025/03/03 11:19:14   Field Name                    |Value 1                       |Value 2                       |Description
-2025/03/03 11:19:14 ------------------------------------------------------------------------------------------------------------------------
-2025/03/03 11:19:14   id2                           |                              |                              |Field does not exists in ./d2
-2025/03/03 11:19:14   herkomst                      |string(1)                     |string(11)                    |Different values
-2025/03/03 11:19:14   straat                        |Straat                        |Straata                       |Different values
-2025/03/03 11:19:14 Comparing ./d2 ==> ./d1
-2025/03/03 11:19:14 Compared files: d2\address.def ==> d1\address.def
-2025/03/03 11:19:14   Field Name                    |Value 1                       |Value 2                       |Description
-2025/03/03 11:19:14 ------------------------------------------------------------------------------------------------------------------------
-2025/03/03 11:19:14   id3                           |                              |                              |Field does not exists in ./d1
-2025/03/03 11:19:14   herkomst                      |string(11)                    |string(1)                     |Different values
-2025/03/03 11:19:14   straat                        |Straata                       |Straat                        |Different values
-```
+![alt text]({B09CA5AC-1425-4998-9717-A5EF5E7B5A34}.png)
 
 - display not existing files
 
@@ -55,12 +32,11 @@ go run main.go -d1 ./d1 -d2 ./d2 -ne
 Result:
 
 ```bash
-2025/03/03 11:19:42 Comparing ./d1 ==> ./d2
-2025/03/03 11:19:42 Compared files: d1\address.def ==> d2\address.def
-2025/03/03 11:19:42   Field Name                    |Value 1                       |Value 2                       |Description
-2025/03/03 11:19:42 ------------------------------------------------------------------------------------------------------------------------
-2025/03/03 11:19:42   id2                           |                              |                              |Field does not exists in ./d2
-2025/03/03 11:19:42   herkomst                      |string(1)                     |string(11)                    |Different values
-2025/03/03 11:19:42   straat                        |Straat                        |Straata                       |Different values
-2025/03/03 11:19:42 File d2\address2 copy.def not exists
+Feature,Field,Dir 1,Dir 2,type (Dir 1),type (Dir 2),external_name (Dir 1),external_name (Dir 2)
+address.def,id2,removed,,,,,
+address.def,id3,,added,,,,
+address.def,herkomst,,,string(1),string(11),,
+address.def,straat,,,,,Straat,Straata
+,,,,,,,
+File d2\address2 copy.def not exists
 ```
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/fs"
 	"log"
 	"os"
@@ -11,27 +13,70 @@ import (
 )
 
 var (
-	fieldsToCheck    = []string{"type", "external_name"}
+	fieldsToCheck = []string{"type", "external_name"}
+	ignoreFields  = "calc__"
 )
 
-type Result struct {
+type ResultBothWay struct {
+	featureName string
 	fieldName string
-	value1    string
-	value2    string
-	reason string
+	d1Fields  map[string]string
+	d2Fields  map[string]string
+	stateInD1 string
+	stateInD2 string
 }
 
-type FeatureDef map[string]interface{}
+type FeatureDef map[string]any
+
+type Exporter struct {
+	writer *csv.Writer
+}
+
+func (e *Exporter) WriteHeader() {
+	row := []string{}
+
+	dir1Name := flag.CommandLine.Lookup("name1").Value.String()
+	dir2Name := flag.CommandLine.Lookup("name2").Value.String()
+	row = append(row, "Feature")
+	row = append(row, "Field")
+	row = append(row, dir1Name)
+	row = append(row, dir2Name)
+	for _, field := range fieldsToCheck {
+		row = append(row, fmt.Sprintf("%s (%s)", field, dir1Name))
+		row = append(row, fmt.Sprintf("%s (%s)", field, dir2Name))
+	}
+	e.writer.Write(row)
+	e.writer.Flush()
+}
+
+func (e *Exporter) WriteSeparator() {
+	rowLength := 4 + len(fieldsToCheck)*2
+	row := make([]string, rowLength)
+	e.writer.Write(row)
+	e.writer.Flush()
+}
+
+func (e *Exporter) WriteRow(result *ResultBothWay) {
+	row := []string{result.featureName, result.fieldName, result.stateInD1, result.stateInD2}
+	for _, fieldName := range fieldsToCheck {
+		row = append(row, result.d1Fields[fieldName])
+		row = append(row, result.d2Fields[fieldName])
+	}
+	e.writer.Write(row)
+	e.writer.Flush()
+}
+
 
 func init() {
 	var (
-		dir1, dir2       string
-		displayNonExists,bothWay bool
-	)	
+		dir1, dir2, dir1Name, dir2Name string
+		displayNonExists               bool
+	)
 	flag.StringVar(&dir1, "d1", "", "Dir 1")
 	flag.StringVar(&dir2, "d2", "", "Dir 2")
+	flag.StringVar(&dir1Name, "name1", "Dir 1", "Dir 1 Name")
+	flag.StringVar(&dir2Name, "name2", "Dir 2", "Dir 2 Name")
 	flag.BoolVar(&displayNonExists, "ne", false, "display not existing files")
-	flag.BoolVar(&bothWay, "bw", false, "Compare both way")
 	flag.Parse()
 }
 
@@ -45,14 +90,11 @@ func main() {
 	dir1 := flag.CommandLine.Lookup("d1").Value.String()
 	dir2 := flag.CommandLine.Lookup("d2").Value.String()
 	displayNonExists := flag.CommandLine.Lookup("ne").Value.String() == "true"
-	bothWay := flag.CommandLine.Lookup("bw").Value.String() == "true"
-	compare(dir1, dir2, displayNonExists)
-	if bothWay {
-		compare(dir2, dir1, displayNonExists)
-	}
+	compareBothWay(dir1, dir2, displayNonExists)
+
 }
 
-func compare(dir1, dir2 string, displayNonExists bool) {
+func compareBothWay(dir1, dir2 string, displayNonExists bool) {
 	var (
 		entries1 []fs.DirEntry
 		entry1   fs.DirEntry
@@ -61,14 +103,14 @@ func compare(dir1, dir2 string, displayNonExists bool) {
 		feature1 FeatureDef
 		feature2 FeatureDef
 	)
-	log.Printf("Comparing %s ==> %s", dir1, dir2)
 	entries1, err = os.ReadDir(dir1)
 	handleErr(err)
+	exporter := &Exporter{writer: csv.NewWriter(os.Stdout)}
+	exporter.WriteHeader()
 	for _, entry1 = range entries1 {
 		if entry1.IsDir() {
 			continue
 		}
-		results := []Result{}
 		name := entry1.Name()
 		filepath1 := filepath.Join(dir1, name)
 		filepath2 := filepath.Join(dir2, name)
@@ -78,60 +120,108 @@ func compare(dir1, dir2 string, displayNonExists bool) {
 		bytes, err = os.ReadFile(filepath2)
 		if os.IsNotExist(err) {
 			if displayNonExists {
-				log.Printf("File %s not exists", filepath2)
+				fmt.Printf("File %s not exists\n", filepath2)
 			}
 			continue
 		}
+		handleErr(err)
 		handleErr(json.Unmarshal(bytes, &feature2))
 		fields1 := feature1["fields"]
 		fields2 := feature2["fields"]
-		res := compareFields(fields1, fields2, dir2)
-		results = append(results, res...)
-		logExport(results, filepath1, filepath2)
+		res := compareFieldsBothWay(fields1, fields2, name)
+		if len(res) > 0 {
+			csvExportBothWay(exporter, res)
+			exporter.WriteSeparator()
+		}
 	}
 }
 
-
-func compareFields(fields1 interface{}, fields2 interface{}, secondDir string) (results []Result) {
-	mFields1, _ := fields1.([]interface{})
-		mFields2, _ := fields2.([]interface{})
-		for _, intFields1 := range mFields1 {
-			field1, _ := intFields1.(map[string]interface{})
-				found := false
-				fieldName1 := field1["name"].(string)
-				for _, intFields2 := range mFields2 {
-					field2, _ := intFields2.(map[string]interface{})
-					fieldName2 := field2["name"].(string)
-					if fieldName1 == fieldName2 {
-						found = true
-						for _, fieldName := range fieldsToCheck {
-							value1 := field1[fieldName].(string)
-							value2 := field2[fieldName].(string)
-							if (value1 != value2) {
-								results = append(results, Result{fieldName: fieldName1, 
-									value1: value1, 
-									value2: value2,
-									reason: "Different values",
-								})
-							}
-						}
-						break
-					}
-				}
-				if !found {
-					results = append(results, Result{fieldName: fieldName1, reason: "Field does not exists in " + secondDir})
-				}
-		}
-		return
+func fieldNames(fields []any) (results []string) {
+	for _, intFields1 := range fields {
+		field1, _ := intFields1.(map[string]any)
+		fieldName1 := field1["name"].(string)
+		results = append(results, fieldName1)
+	}
+	return
 }
 
-func logExport(results []Result, filepath1, filepath2 string){
-	if len(results) > 0 {
-		log.Printf("Compared files: %s ==> %s", filepath1, filepath2)
-		log.Printf("  %-30s|%-30s|%-30s|%-30s", "Field Name", "Value 1", "Value 2", "Description")	
-		log.Printf("%s", strings.Repeat("-", 120))	
-		for _, s := range results {
-			log.Printf("  %-30s|%-30s|%-30s|%-30s", s.fieldName, s.value1, s.value2, s.reason)	
+func getFieldDef(fields []any, fieldName string) map[string]any {
+	for _, intFields1 := range fields {
+		field1, _ := intFields1.(map[string]any)
+		fieldName1 := field1["name"].(string)
+		if fieldName1 == fieldName {
+			return field1
 		}
+	}
+	return nil
+}
+
+func compareFieldsBothWay(fields1 any, fields2 any, featureName string) (results []*ResultBothWay) {
+	allFields := map[string]*ResultBothWay{}
+
+	mFields1, _ := fields1.([]any)
+	mFields2, _ := fields2.([]any)
+	fields := fieldNames(mFields1)
+	fields = append(fields, fieldNames(mFields2)...)
+
+	for _, fieldName := range fields {
+		if strings.HasPrefix(fieldName, ignoreFields) {
+			continue
+		}
+		allFields[fieldName] = &ResultBothWay{
+			fieldName: fieldName,
+			featureName: featureName,
+			d1Fields:  map[string]string{},
+			d2Fields: map[string]string{},
+		}
+	}
+
+	for fieldName, result := range allFields {
+		field1 := getFieldDef(mFields1, fieldName)
+		field2 := getFieldDef(mFields2, fieldName)
+		if field1 == nil && field2 != nil {
+			result.stateInD2 = "added"
+			results = append(results, result)
+			continue
+		}
+		if field1 != nil && field2 == nil {
+			result.stateInD1 = "removed"
+			results = append(results, result)
+			continue
+		}
+		
+		different := false
+		for _, fieldName := range fieldsToCheck {
+			if field1[fieldName] != nil && 
+				field2[fieldName] != nil && 
+				field1[fieldName].(string) != field2[fieldName].(string) {
+				result.d1Fields[fieldName] = field1[fieldName].(string)
+				result.d2Fields[fieldName] = field2[fieldName].(string)
+				different = true
+			}
+		}
+		if different {
+			results = append(results, result)
+		}
+	}
+	return
+}
+
+func csvExportBothWay(exporter *Exporter, results []*ResultBothWay) {
+	for _, result := range results {
+		if result.stateInD1 != "" {
+			exporter.WriteRow(result)
+		}
+	}
+	for _, result := range results {
+		if result.stateInD2 != "" {
+			exporter.WriteRow(result)
+		}
+	}
+	for _, result := range results {
+		if result.stateInD1 != "" || result.stateInD2 != "" {
+			continue
+		}
+		exporter.WriteRow(result)
 	}
 }
